251914fcabecf2a4906782255e4401da17a45c5a
llin31 <llin31@horizon.csueastbay.edu>
2021-03-14 16:34:11 -0700

    commit 2

--- practice/processIt.js
+++ practice/processIt.js
@@ -1,18 +1,29 @@
+// 1c
 const fs = require('fs');
 const readline = require('readline');
 
-let fileName = _dirname + '/about.md';
+let fileName = __dirname + '/about.md';
 let fileData = fs.readFileSync(fileName, 'utf8');
+let temp = fileData.split("\n");
 
-const readInterface = readline.createInterface
-({
-    input: fs.createReadStream(fileName),
-})
+console.log(temp.length - 1);
 
-let lineCount = 0;
-readInterface.on('line', function(line)
+// 1e
+fileName = __dirname + '/eventData.json';
+fileData = fs.readFileSync(fileName, 'utf8');
+temp = JSON.parse(fileData);
+
+let myArray = [];
+
+for(let i in temp.events)
 {
-    lineCount++;
-});
+    myArray.push
+    ({
+        name: temp.events[i].name,
+        date: temp.events[i].date,
+        description: temp.events[i].description
+    });
+}
 
-console.log(lineCount);
+console.log(myArray);
+
